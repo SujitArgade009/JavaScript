a1b864b8dc6453ba5c534f58495464d13ebe793d
sujitargade009 <sujitargade009@gmail.com>
2023-10-23 11:49:39 +0530

    basic changes

---
+++ index7.js
@@ -0,0 +1,8 @@
+// Accessing the Element by selcting and element id:
+
+document.getElementById("mainImg");
+let ImgObj=document.getElementById("mainImg");
+console.log(ImgObj);
+
+// manupilation of the src data by your own :
+// ImgObj.src="assets-20231023T052556Z-001/assets/creation_1.png"
