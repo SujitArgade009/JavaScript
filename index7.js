c72c57f344f08da9c1d0c493032c02be4f098003
Sujit Argade <144983208+SujitArgade009@users.noreply.github.com>
2023-10-26 12:37:50 +0530

    Update index7.js

--- index7.js
+++ index7.js
@@ -6,3 +6,10 @@ console.log(ImgObj);
 
 // manupilation of the src data by your own :
 // ImgObj.src="assets-20231023T052556Z-001/assets/creation_1.png"
+let img=document.getElementById("mainImg");
+console.log(img); 
+console.dir(img); // you will get to see the indiviual properties of the img element in document
+
+let ok=document.getElementById("description");
+console.log(ok);
+console.dir(ok); // dir is the function which will give you the all properties of the that id like what is that element
