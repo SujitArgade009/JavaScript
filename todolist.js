00eb83dd6651ae77b747e3e7ef9cc6d3926ad7e7
Sujit Argade <144983208+SujitArgade009@users.noreply.github.com>
2023-10-31 17:17:23 +0530

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,1 +1,42 @@
+;let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+let input=document.querySelector("input");
 
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=input.value;
+
+    
+    let delbtn=document.createElement("button");
+    delbtn.innerText="Delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    input.value="";
+});
+
+
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem=event.target.parentElement;
+       listItem.remove();
+        console.log("Deleted");
+    }
+});
+
+
+
+// this delete button Property is delete the button of the already we have in the list:
+// So we need to create event Listners to the parent element so we can trigger the operation using the parent Element
+// let delbtns=document.querySelectorAll(".delete");
+
+// for(del of delbtns){
+//     del.addEventListener("click",function(){
+//         let parent=this.parentElement;
+//         console.log(parent);
+//         parent.remove();
+//         console.log("Element Deleted");
+//     });
+// }
