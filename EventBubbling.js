5bdbeb9374e4ddb5e08b50704e21081e2626d8f9
Sujit Argade <144983208+SujitArgade009@users.noreply.github.com>
2023-10-31 18:10:32 +0530

    Update EventBubbling.js

--- EventBubbling.js
+++ EventBubbling.js
@@ -1,1 +1,23 @@
+// This Code Showing the Event Bubbling: means how will Event Bubbling happens in the children Parent Connections
+
+// For stop event Bubbling use one method called stop Propagation:
+let div=document.querySelector("div");
+let ul=document.querySelector("ul");
+let lis=document.querySelectorAll("li");
+
+div.addEventListener("click",function(){
+    console.log("Div Was Clicked");
+});
+
+ul.addEventListener("click",function(event){
+    event.stopPropagation();
+    console.log("ul Was Clicked");
+});
+
+for (li of lis){
+    li.addEventListener("click",function(event) {
+        event.stopPropagation();
+        console.log("li Was Clicked");
+    });
+}
 
